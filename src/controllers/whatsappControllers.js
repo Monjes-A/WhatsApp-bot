c7255bacee1814bd1aee22ad65badd0288830b7d
Monjez <Jesusjesusmonsalve1414@gmail.com>
2023-09-29 20:49:47 -0400

    activando verificador de token

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,19 @@
 const verifyToken = (req, res) => {
-    res.send("hola verifyToken");
+    
+    try{
+        var accessToken = "6vDZG2vWROha6sZV81fBgjCGzIFDEPp2NTwmMo8Wixw6/lUGQjb/j/!IjEeHVXhc";
+        var token = req.query["hub.verify_token"];
+        var challenge = req.query["hub.challenge"];
+        
+        if(challenge != null && token != null && token == accessToken){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }        
+    }
+    catch(e){
+        res.status(400).send();
+    }
 }
 
 const ReceivedMessage = (req, res) => {
